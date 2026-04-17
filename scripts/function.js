567607aa6ee5e10c6dc4689d8091bedabf38d16a
Francesco Cassese <francescocassese9@gmail.com>
2026-04-17 12:12:19 +0200

    feat: aggiunta funzione chiamataApi

--- scripts/function.js
+++ scripts/function.js
@@ -1,2 +1,23 @@
 //@ts-check
 'use strict';
+
+/*
+================================================================
+    FUNZIONE CHIAMATA API
+================================================================
+*/
+
+const chiamataApi = () => {
+    return fetch('https://lanciweb.github.io/demo/api/pictures/')  //faccio la richiesta al server
+        .then(response => {                                        //appena risponde trasformo il dato grezzo in json
+            console.log(response);
+            return response.json();                                //restituisco i dati in json
+        })
+        .then(json => {                                            //una volta che ha trasformato i dati in json
+            console.log(json);
+            return json;                                           //restituisco il dato intero in json
+        })
+        .catch(errore => {                                         //se c'è un errore
+            console.error(errore);                                 //momentaneamente lo stampo in console
+        })
+}
