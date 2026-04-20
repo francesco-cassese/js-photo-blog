ad55f6b3d86cc8e7fc6dc979f4aff25d94aeb2fb
Francesco Cassese <francescocassese9@gmail.com>
2026-04-20 17:04:44 +0200

    refactor: ottimizzazione codice

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,8 @@ const galleryHtml = document.querySelector('#photo-gallery');
 const containerOverlay = document.querySelector('.img-overlay-container');
 const imgOverlay = document.querySelector('.img-overlay');
 const bottoneCloseOverlay = document.querySelector('.btn-close-overlay');
-const moduloInvio = document.querySelector('form');
-const selettoreFile = document.getElementById('upload-img');
+const moduloInvio = document.querySelector('#form');
+const selettoreFile = document.querySelector('#upload-img');
 
 stampaListaCompleta();                                             //Faccio partire la generazione delle card dall'API
 
