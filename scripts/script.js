231909d8435300b0e9103451bab6143ed60f3d7b
Francesco Cassese <francescocassese9@gmail.com>
2026-04-20 15:12:14 +0200

    feat: richiamate funzioni in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,20 @@
 'use strict';
 
 const galleryHtml = document.querySelector('#photo-gallery');
+const containerOverlay = document.querySelector('.img-overlay-container');
+const imgOverlay = document.querySelector('.img-overlay');
+const bottoneCloseOverlay = document.querySelector('.btn-close-overlay');
 
-stampaListaCompleta();
+stampaListaCompleta();                                             //Faccio partire la generazione delle card dall'API
+
+galleryHtml?.addEventListener('click', (evento => {
+    if (containerOverlay && imgOverlay) {
+        gestisciAperturaOverlay(evento, containerOverlay, imgOverlay)  //Quando clicco su una card, chiamo la funzione per aprire
+    }
+}));
+
+bottoneCloseOverlay?.addEventListener('click', () => {
+    if (containerOverlay) {
+        nascondiOverlay(containerOverlay)                              //Quando clicco sul bottone "Chiudi", chiamo la funzione per chiudere
+    }
+}); 
